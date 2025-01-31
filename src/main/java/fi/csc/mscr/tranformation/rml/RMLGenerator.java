cb3b0e05b9f2496ee7ac8398bda63c36f65d2840
Tobias Schweizer <tobias.schweizer@switch.ch>
2025-01-31 08:30:26 +0100

    chore (add deps): add deps to build.gradle

--- src/main/java/fi/csc/mscr/tranformation/rml/RMLGenerator.java
+++ src/main/java/fi/csc/mscr/tranformation/rml/RMLGenerator.java
@@ -1,5 +1,16 @@
 package fi.csc.mscr.tranformation.rml;
 
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Literal;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.vocabulary.RDF;
+import org.topbraid.shacl.vocabulary.SH;
+
 public class RMLGenerator {
 
 	public String testMe() {
